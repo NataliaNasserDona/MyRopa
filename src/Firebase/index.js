31cb940995ffff859aa32f7f3ce8f16289ccfd30
NataliaNasserDona <91989549+NataliaNasserDona@users.noreply.github.com>
2021-12-10 17:15:39 -0300

    mas

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,14 +1,14 @@
 import {initializeApp} from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import {collection, addDoc, getDoc, doc, Timestamp, writeBatch, query, where, getDocs} from 'firebase/firestore'
+import {collection, addDoc, getDoc, doc, Timestamp, writeBatch} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
+  apiKey: process.env.REACT_APP_appKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig)
